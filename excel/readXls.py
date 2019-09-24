ad99f21bad7476128b5ab304eb3061fa04d24178
georstoy <georstoy@gmail.com>
2019-09-24 21:37:35 +0300

    Solution: parse Excel data

--- excel/readXls.py
+++ excel/readXls.py
@@ -22,48 +22,38 @@ def open_zip(datafile):
 
 def parse_file(datafile):
     workbook = xlrd.open_workbook(datafile)
-    sheet = workbook.sheet_by_index(0)
+    sheet = workbook.sheet_by_index(0) # get first sheet
 
-    ### example on how you can get the data
-    #sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+# find and return the min, max and average values for the COAST region
+    # get data from the COAST col
+    COAST_COL = 1
+    coast_data = [sheet.cell_value(r, COAST_COL) 
+                    for r in range(1, sheet.nrows)]
 
-    ### other useful methods:
-    # print "\nROWS, COLUMNS, and CELLS:"
-    # print "Number of rows in the sheet:", 
-    # print sheet.nrows
-    # print "Type of data in cell (row 3, col 2):", 
-    # print sheet.cell_type(3, 2)
-    # print "Value in cell (row 3, col 2):", 
-    # print sheet.cell_value(3, 2)
-    # print "Get a slice of values in column 3, from rows 1-3:"
-    # print sheet.col_values(3, start_rowx=1, end_rowx=4)
+    minvalue = min(coast_data)
+    maxvalue = max(coast_data)
 
-    # print "\nDATES:"
-    # print "Type of data in cell (row 1, col 0):", 
-    # print sheet.cell_type(1, 0)
-    # exceltime = sheet.cell_value(1, 0)
-    # print "Time in Excel format:",
-    # print exceltime
-    # print "Convert time to a Python datetime tuple, from the Excel float:",
-    # print xlrd.xldate_as_tuple(exceltime, 0)
-    
-    
+# find and return the time value for the min and max entries
+    # get data from the Hour_End col
+    TIME_COL = 0
+    time_data = [sheet.cell_value(r, TIME_COL)
+                    for r in range(1, sheet.nrows)]
+
     data = {
-            'maxtime': (0, 0, 0, 0, 0, 0),
-            'maxvalue': 0,
-            'mintime': (0, 0, 0, 0, 0, 0),
-            'minvalue': 0,
-            'avgcoast': 0
+            'maxtime': xlrd.xldate_as_tuple(time_data[coast_data.index(maxvalue)], workbook.datemode),
+            'maxvalue': round(maxvalue,10),
+            'mintime': xlrd.xldate_as_tuple(time_data[coast_data.index(minvalue)], workbook.datemode),
+            'minvalue': round(minvalue,10),
+            'avgcoast': round(sum(coast_data)/len(coast_data),10)
     }
     return data
 
 
 def test():
-    open_zip(datafile)
+    open_zip(datafile.lower().rstrip('.xls').replace('_','-'))
     data = parse_file(datafile)
 
     assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
     assert round(data['maxvalue'], 10) == round(18779.02551, 10)
-
 
 test()
